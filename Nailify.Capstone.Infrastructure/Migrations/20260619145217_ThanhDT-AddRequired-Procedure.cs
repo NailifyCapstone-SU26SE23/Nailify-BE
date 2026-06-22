501e321ca01df7e4615ef7a8e4a472110b3b26df
Phan Do Gia Tue <phandogiatue51@gmail.com>
2026-06-22 12:46:52 +0700

    Refactor DI and remove IsRequired migration

--- Nailify.Capstone.Infrastructure/Migrations/20260619145217_ThanhDT-AddRequired-Procedure.cs
+++ Nailify.Capstone.Infrastructure/Migrations/20260619145217_ThanhDT-AddRequired-Procedure.cs
@@ -10,31 +10,13 @@ namespace Nailify.Capstone.Infrastructure.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<bool>(
-                name: "IsRequired",
-                table: "Procedures",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false);
-
-            migrationBuilder.AddColumn<bool>(
-                name: "IsRequired",
-                table: "BookingProcedures",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false);
+      
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "IsRequired",
-                table: "Procedures");
 
-            migrationBuilder.DropColumn(
-                name: "IsRequired",
-                table: "BookingProcedures");
         }
     }
 }
