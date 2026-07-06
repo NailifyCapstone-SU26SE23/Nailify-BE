c65e0c2d3d4f6d55fb98b1037e6cf4347a6adde5
Phan Do Gia Tue <phandogiatue51@gmail.com>
2026-07-06 20:56:42 +0700

    fix shapemethod dto

--- Nailify.Capstone.Application/DTOs/ResponseDTOs/ShapeMethodConfigDto.cs
+++ Nailify.Capstone.Application/DTOs/ResponseDTOs/ShapeMethodConfigDto.cs
@@ -8,16 +8,10 @@ namespace Nailify.Capstone.Application.DTOs.ResponseDTOs
     {
         public int ShapeMethodConfigId { get; set; }
         public int NailShapeId { get; set; }
-        public string NailShapeName { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public int Duration { get; set; }
         public string Status { get; set; } = string.Empty;
 
-        public void Mapping(Profile profile)
-        {
-            profile.CreateMap<ShapeMethodConfig, ShapeMethodConfigDto>()
-                .ForMember(dest => dest.NailShapeName, opt => opt.MapFrom(src => src.NailShape != null ? src.NailShape.Name : ""));
-        }
     }
 }
