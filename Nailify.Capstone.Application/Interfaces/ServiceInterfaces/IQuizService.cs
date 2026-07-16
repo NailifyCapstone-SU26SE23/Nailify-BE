137897466dd011f73d6002c64b5df500811992a8
erik-dev <12b4thanh@gmail.com>
2026-07-16 13:25:03 +0700

    fix: quiz question flexible & machine learning

--- Nailify.Capstone.Application/Interfaces/ServiceInterfaces/IQuizService.cs
+++ Nailify.Capstone.Application/Interfaces/ServiceInterfaces/IQuizService.cs
@@ -1,10 +1,9 @@
-﻿using Nailify.Capstone.Application.Common;
+using Nailify.Capstone.Application.Common;
 using Nailify.Capstone.Application.DTOs.RequestDTOs.QuizRequestDTOs;
 using Nailify.Capstone.Application.DTOs.ResponseDTOs.QuizResponseDTOs;
+using Nailify.Capstone.Domain.Enums;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Nailify.Capstone.Application.Interfaces.ServiceInterfaces
@@ -12,8 +11,10 @@ namespace Nailify.Capstone.Application.Interfaces.ServiceInterfaces
     public interface IQuizService
     {
         Task<ApiResult<List<StyleQuizQuestionResponseDTO>>> GetQuizQuestionsAsync();
-        Task<ApiResult<StyleQuizQuestionResponseDTO>> CreateQuestionAsync(CreateQuizQuestionRequestDTO request);
-        Task<ApiResult<StyleQuizQuestionResponseDTO>> UpdateQuestionAsync(Guid questionId, UpdateQuizQuestionRequestDTO request);
+        Task<ApiResult<StyleQuizQuestionResponseDTO>> CreateQuestionAsync(QuizQuestionType type, QuizCategory category, CreateQuizQuestionRequestDTO request);
+        Task<ApiResult<StyleQuizQuestionResponseDTO>> UpdateQuestionAsync(Guid questionId, QuizQuestionType type, QuizCategory category, UpdateQuizQuestionRequestDTO request);
         Task<ApiResult<bool>> DeleteQuestionAsync(Guid questionId);
+        Task<ApiResult<StyleQuizQuestionResponseDTO>> AddOptionToQuestionAsync(Guid questionId, AddQuizOptionRequestDTO request);
+        Task<ApiResult<bool>> DeleteOptionAsync(Guid optionId);
     }
 }
