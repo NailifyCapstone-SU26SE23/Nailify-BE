8fee1819fb28d1fe70abae8b5d4d9a59a2154b10
Phan Do Gia Tue <phandogiatue51@gmail.com>
2026-07-16 18:53:22 +0700

    Fix loyaltyTier rule

---
+++ Nailify.Capstone.Infrastructure/Migrations/20260716115150_TuePDG-FixLoyaltyTierRule.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nailify.Capstone.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TuePDGFixLoyaltyTierRule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_LoyaltyTiers_MinLifetimePoints",
+                table: "LoyaltyTiers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_LoyaltyTiers_MinLifetimePoints",
+                table: "LoyaltyTiers",
+                column: "MinLifetimePoints",
+                unique: true);
+        }
+    }
+}
