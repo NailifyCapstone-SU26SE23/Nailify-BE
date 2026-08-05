ed9f6d776f391d242ba728f12d0f79e227710c09
Phan Do Gia Tue <phandogiatue51@gmail.com>
2026-08-06 01:30:14 +0700

    fix default email

--- Nailify.Capstone.Infrastructure/Configuration/SendGridEmailConfiguration.cs
+++ Nailify.Capstone.Infrastructure/Configuration/SendGridEmailConfiguration.cs
@@ -3,7 +3,7 @@ namespace Nailify.Capstone.Infrastructure.Configuration
     public class SendGridEmailConfiguration
     {
         public string ApiKey { get; set; } = string.Empty;
-        public string From { get; set; } = "nailify.noreply@gmail.com";
+        public string From { get; set; } = "nailify.capstone@gmail.com";
         public string DisplayName { get; set; } = "Nailify Center";
     }
 }
